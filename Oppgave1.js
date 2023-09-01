f12561d0237c39cb136b03c21d501cab4a0dd353
desireekodehode <desireekodehode1@gmail.com>
2023-09-01 13:12:22 +0200

    '

--- Oppgave1.js
+++ Oppgave1.js
@@ -1,3 +1,13 @@
-let ord = "Hello"
-let ord2 = "World"
-console.log(ord + ord2)
+let ord1 = "Hello"
+let ord2 = " World"
+console.log(ord1 + ord2)
+
+let ord3 = "Dette er en stening "
+let ord4 = " den skal ut i verden!"
+console.log(ord3 + ord4)
+
+let age = 18
+
+age >= 18 ? console.log("Barn") : console.log("Du er ikke barn lengre")
+
+
